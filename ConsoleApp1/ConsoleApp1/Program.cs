b1b620b0b77a529c1a8c393331d299167675042f
raulgonzalezlozoya <98315234+raulgonzalezlozoya@users.noreply.github.com>
2022-01-26 10:48:07 +0100

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(int numeroDeEstrellas)
+        {
+            for(i=0;i<numeroDeEstrellas;i++)
+            {
+
+
+
+                Console.Write("*");
+            }
+            Console.Write("\n");
+            {
+
+
+
+
+            }
+
+
+
+
+        }
+    }
+}
