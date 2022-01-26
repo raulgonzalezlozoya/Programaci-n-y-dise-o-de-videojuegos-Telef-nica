5674ec7af3974899287b6476620cda798ee088fc
raulgonzalezlozoya <98315234+raulgonzalezlozoya@users.noreply.github.com>
2022-01-26 11:19:32 +0100

    Add files via upload

---
+++ Colisiones/Enemigo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enemigo : MonoBehaviour {
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(other.gameObject, 1.5f);
+            SceneManager.LoadScene("desencadenadores");
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
