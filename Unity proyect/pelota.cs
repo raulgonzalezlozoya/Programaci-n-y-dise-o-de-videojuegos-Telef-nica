b64996f6bb3a70a50388e97ecfbeffac5db9bc1c
raulgonzalezlozoya <98315234+raulgonzalezlozoya@users.noreply.github.com>
2022-01-26 11:22:58 +0100

    Add files via upload

---
+++ Unity proyect/pelota.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pelota : MonoBehaviour {
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            Debug.Log("Tocada pelota " + collision.transform.name);
+        }
+
+
+
+
+
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+	}
+}
