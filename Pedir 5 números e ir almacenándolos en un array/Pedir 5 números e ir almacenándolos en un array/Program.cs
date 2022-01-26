4cca31187000aad1ab539a2411d63057aa31cfd2
raulgonzalezlozoya <98315234+raulgonzalezlozoya@users.noreply.github.com>
2022-01-26 11:15:16 +0100

    Add files via upload

---
+++ Pedir 5 números e ir almacenándolos en un array/Pedir 5 números e ir almacenándolos en un array/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pedir_5_números_e_ir_almacenándolos_en_un_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+    }
+}
