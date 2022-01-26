e0611f8733f936f625a00ee7845c86aa46c7ad94
raulgonzalezlozoya <98315234+raulgonzalezlozoya@users.noreply.github.com>
2022-01-26 11:22:31 +0100

    Add files via upload

---
+++ Unity proyect/enemigo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemigo : MonoBehaviour {
+
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.transform.Translate(Vector3.forward * this.speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Vector3.forward * this.speed);
+    }
+}
---
+++ Unity proyect/sounds.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sounds : MonoBehaviour {
+
+
+    public AudioClip bug;
+
+
+
+
+
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+       
+        // AudioSource.PlayClipAtPoint(this.bug, this.transform.position);
+       // AudioSource.PlayClipAtPoint(this.bug, Camera.main.transform.position);
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            this.GetComponent<AudioSource>().clip = this.bug;
+
+
+
+
+        }
+    }
+}
