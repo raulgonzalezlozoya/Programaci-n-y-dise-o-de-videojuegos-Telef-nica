ab369fc778e88f811143e9cd117bc79c71117023
raulgonzalezlozoya <98315234+raulgonzalezlozoya@users.noreply.github.com>
2022-01-26 11:04:56 +0100

    Add files via upload

---
+++ Crear un array con los nombres de los planetas/Crear un array con los nombres de los planetas/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crear_un_array_con_los_nombres_de_los_planetas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //arrays
+            int[] listaNumeros;
+            listaNumeros = new int[5];
+            Console.WriteLine("introduce cinco numeros: ");
+
+            for(int i=0;i<5;i++)
+            {
+
+                listaNumeros[i] = int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine("contenido del array: ");
+            for (int i=0;i<5;i++)
+            {
+
+                Console.WriteLine(listaNumeros[i]);
+
+
+            }
+            
+
+        }
+    }
+}
